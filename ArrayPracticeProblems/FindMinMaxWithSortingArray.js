1451fda09b2a0ca7213ae54103b38bab1360c6a5
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-03-24 22:36:18 +0530

    Updated : Generate 10 random numbers and find min and max without sorting array

---
+++ ArrayPracticeProblems/FindMinMaxWithSortingArray.js
@@ -0,0 +1,13 @@
+var storeNumbers = [];
+let min = 100;
+let max = 999;
+for(let i = 1;i<=10;i++){
+    let value = Math.floor(Math.random()*(max-min+1)+min);
+    storeNumbers.push(value);
+}
+console.log(storeNumbers.sort());
+console.log("First Max: "+ storeNumbers[storeNumbers.length-1]+"\n"+
+            "Second Max : "+storeNumbers[storeNumbers.length-2]);
+            
+console.log("First Min: "+ storeNumbers[0]+"\n"+
+            "Second Min : "+storeNumbers[1]);
